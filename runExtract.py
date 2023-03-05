4c68ab85de520fb906b5464d4322a4cd2519c08d
****# <earth@mail.ustc.edu.cn>
2023-03-05 22:19:01 +0800

    update run

--- runExtract.py
+++ runExtract.py
@@ -7,20 +7,29 @@ import time,os
 视频缩小了一倍进行存储的
 """
 
-# data_folder = "G:/WenLabData/ExtractWormPoseData/xrk/N2_Free_Moving/YamlFiles"
+data_folder = "G:/WenLabData/ExtractWormPoseData/xrk/N2_Free_Moving/YamlFiles"
 
-data_folder = "G:/WenLabData/ExtractWormPoseData/Colbert-hpis258/YamlFiles"
+
+
 SaveFolder = os.path.join(data_folder,'ExtractData')
 
 YamlExportMatAll(data_folder,SaveFolder=SaveFolder,
-                 Centerline=True,
-                 curvedatafiltered=True,
-                 TimeElapsed=True,
-                 Head=True,
-                 mean_worm_length=True,
-                 name=True,
-                 worm_length=True,
-                 savevideo=False,
+                ExperimentTime=True,
+                BoundaryA=True,
+                BoundaryB=True,
+                Centerline=True,
+                curvedatafiltered=True,
+                TimeElapsed=True,
+                angle_data=True,
+                curve_data=True,
+                mean_worm_length=True,
+                StageFeedbackTarget=True,
+                StagePosition=True,
+                StageVelocity=True,
+                Head=True,
+                name=True,
+                worm_length=True,
+                savevideo=False,
                  )
 
 
